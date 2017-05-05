eb4c8bef29d22923aa71d8d93783896843ff0eab
Ben Constable <ben.constable9@btinternet.com>
2017-05-05 23:24:12 +0100

    Updated main.js
    Fixed index.html not loading. Added close events

--- main.js
+++ main.js
@@ -4,11 +4,35 @@ const electron = require('electron');
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
 var mainWindow = null;
+
+const path = require('path')
+const url = require('url')
+
 app.on('ready', function(){
     mainWindow = new BrowserWindow({
         width: 612,
         height: 384
     });
     mainWindow.webContents.openDevTools();
-    mainWindow.loadURL('file://${_dirname}/app/index.html');
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'app/index.html'),
+        protocol: 'file:',
+        slashes: true
+    }))
+    mainWindow.on('closed', function () {
+        mainWindow = null
+    })
+})
+
+app.on('window-all-closed', function () {
+    //mac os
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
+
+app.on('activate', function () {
+    if (mainWindow === null) {
+        createWindow()
+    }
 })
