e5939c9f6c6dcdaa8b317160420371cd9c4c2f02
Ben Constable <ben-constable@outlook.com>
2017-10-08 21:42:46 +0100

    Updates

--- app/js/app.js
+++ app/js/app.js
@@ -11,92 +11,108 @@ var selected_howler;
 
 // ** FUNCTIONS **
 
+function show_alert(message) {
+    if ($('#alert').length == 0) {
+        var alertHtml = "<div id='alert' class='alert alert-danger alert-dismissible alert-fixed' role='alert' style='display:none'><button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button><strong>Warning - Critical Error!</strong> <span id='alertMessage'></span></div>"
+        $("#alertdiv").html(alertHtml);
+        $("#alert").show();
+    }
+    $("#alertMessage").append("<br>" + message);
+}
+
 function add_music_cue(src) {
     src = src.replace(/\\/g, "/");
     console.log(src);
-    $.get(src, function(data, textStatus) {
-        if (textStatus == "success") {
-            //create the howl
-            var sound = new Howl({
-                src: [src],
-                preload: true,
-            });
-            //this adds the music to the list of songs (position is defaulted to last)
-            var cue_list = document.getElementById("cue_list_body");
-            //make sure to add them with the class cue
-            num_cues += 1;
-            var cue_id = num_cues;
-            sound.on('end', function(){
-                document.getElementById(num_cues).classList.remove("success");
-                if (selected_cue.id == cue_id) {
-                    document.getElementById(num_cues).classList.add("info");
-                    document.getElementById("go").disabled = false;
-                    document.getElementById("go").style.display = "block";
-                    document.getElementById("fade").style.display = "none";
-                    document.getElementById("stop").disabled = true;
-                    document.getElementById("pause").disabled = true;
-                    document.getElementById("play").disabled = true;
-                }
-                var cell_id = cue_id + "_";
-                var status_id = cell_id + "status";
-                document.getElementById(status_id).innerHTML = "Ready"; 
-            });
-            sound.once('load', function(){
-                //add the time
-                var duration = sound.duration();
-                duration = (duration / 60).toFixed(2);
-                duration = duration.replace(".", ":");
-                var cell_id = cue_id + "_";
-                var duration_id = cell_id + "duration";
-                var status_id = cell_id + "status";
-                document.getElementById(duration_id).innerHTML = duration;
-                document.getElementById(status_id).innerHTML = "Ready";
-            });
-            if (!howl_objects[num_cues]) {
-                howl_objects[num_cues] = [];
+    var request = $.get(src);
+    request.done(function(result) {
+        //create the howl
+        var sound = new Howl({
+            src: [src],
+            preload: true,
+        });
+        //this adds the music to the list of songs (position is defaulted to last)
+        var cue_list = document.getElementById("cue_list_body");
+        //make sure to add them with the class cue
+        num_cues += 1;
+        var cue_id = num_cues;
+        sound.on('end', function(){
+            document.getElementById(num_cues).classList.remove("success");
+            if (selected_cue.id == cue_id) {
+                document.getElementById(num_cues).classList.add("info");
+                document.getElementById("go").disabled = false;
+                document.getElementById("go").style.display = "block";
+                document.getElementById("fade").style.display = "none";
+                document.getElementById("stop").disabled = true;
+                document.getElementById("pause").disabled = true;
+                document.getElementById("play").disabled = true;
             }
-            howl_objects[num_cues].push(sound);
-            console.log(howl_objects);
-            var cue = cue_list.insertRow(-1);
-            cue.setAttribute("id", cue_id);
-            cue.setAttribute("class", "cue");
-            var title = src;
-            var n = title.lastIndexOf('/');
-            if (n !== -1) {
-                var result = title.substring(n + 1);
-                title = result.replace(/\.[^/.]+$/, "");
-                var status = "Loading...";
-                var duration = "Calculating..";
-                var elements = [cue_id, title, duration, status];
-                for (var y = 0; y < elements.length; ++y) {
-                    //add cell and create text
-                    var cell = cue.insertCell(y);
-                    var name = "";
-                    if (y == 0) {
-                        name = "num";
-                    }
-                    else if (y == 1) {
-                        name = "title";
-                    }
-                    else if (y == 2) {
-                        name = "duration";
-                    }
-                    else if (y == 3) {
-                        name = "status";
-                    }
-                    var cell_id = cue_id + "_" + name;
-                    cell.setAttribute("id", cell_id);
-                    cell.innerHTML = elements[y];
+            var cell_id = cue_id + "_";
+            var status_id = cell_id + "status";
+            document.getElementById(status_id).innerHTML = "Ready"; 
+        });
+        sound.on('loaderror', function(id, error){
+            var cell_id = cue_id + "_";
+            var status_id = cell_id + "status";
+            var duration_id = cell_id + "duration";
+            document.getElementById(status_id).innerHTML = "Error"; 
+            document.getElementById(duration_id).innerHTML = "00:00:00";
+        });
+        sound.once('load', function(){
+            //add the time
+            var duration = sound.duration();
+            duration = (duration / 60).toFixed(2);
+            duration = duration.replace(".", ":");
+            var cell_id = cue_id + "_";
+            var duration_id = cell_id + "duration";
+            var status_id = cell_id + "status";
+            document.getElementById(duration_id).innerHTML = duration;
+            document.getElementById(status_id).innerHTML = "Ready";
+        });
+        if (!howl_objects[num_cues]) {
+            howl_objects[num_cues] = [];
+        }
+        howl_objects[num_cues].push(sound);
+        console.log(howl_objects);
+        var cue = cue_list.insertRow(-1);
+        cue.setAttribute("id", cue_id);
+        cue.setAttribute("class", "cue");
+        var title = src;
+        var n = title.lastIndexOf('/');
+        if (n !== -1) {
+            var result = title.substring(n + 1);
+            title = result.replace(/\.[^/.]+$/, "");
+            var status = "Loading...";
+            var duration = "Calculating..";
+            var elements = [cue_id, title, duration, status];
+            for (var y = 0; y < elements.length; ++y) {
+                //add cell and create text
+                var cell = cue.insertCell(y);
+                var name = "";
+                if (y == 0) {
+                    name = "num";
                 }
-                //todo add the event listeners
-                document.getElementById(cue_id).addEventListener("click", function() {
-                    select_cue(cue_id);
-                });
+                else if (y == 1) {
+                    name = "title";
+                }
+                else if (y == 2) {
+                    name = "duration";
+                }
+                else if (y == 3) {
+                    name = "status";
+                }
+                var cell_id = cue_id + "_" + name;
+                cell.setAttribute("id", cell_id);
+                cell.innerHTML = elements[y];
             }
+            //todo add the event listeners
+            document.getElementById(cue_id).addEventListener("click", function() {
+                select_cue(cue_id);
+            });
         }
-        else {
-            console.error("Cannot Add Cue - File Doesn't exist");
-        }
+    });
+    request.fail(function(jqXHR, textStatus, errorThrown) {
+        var error = "Unable to load file - " + src;
+        show_alert(error);
     });
 }
 
@@ -163,6 +179,7 @@ function stop_cue() {
     var cue = howl_objects[cue_id][0];
     cue.stop();
     document.getElementById(cue_id).classList.remove("success");
+    document.getElementById(cue_id).classList.remove("danger");
     document.getElementById(cue_id).classList.add("info");
     document.getElementById("go").disabled = false;
     document.getElementById("go").style.display = "block";
@@ -180,7 +197,6 @@ function pause_cue() {
     var cue = howl_objects[cue_id][0];
     cue.pause();
     document.getElementById(cue_id).classList.remove("success");
-    document.getElementById(cue_id).classList.remove("danger");
     document.getElementById(cue_id).classList.add("danger");
     document.getElementById("go").disabled = true;
     document.getElementById("go").style.display = "block";
@@ -255,9 +271,8 @@ $("#cue_import_file").change(function(){
 
 //manual adding
 
-add_music_cue("C:/Users/Ben/Downloads/The Wombats - Jump Into The Fog.mp3");
-add_music_cue("C:/Users/Ben/Downloads/The Wombats - Jump Into The Fog.mp3");
-add_music_cue("C:/Users/Ben/Downloads/The Wombats - Jump Into The Fog.mp3");
-add_music_cue("C:/Users/Ben/Downloads/The Wombats - Jump Into The Fog.mp3");
-add_music_cue("C:/Users/Ben/Downloads/The Wombats - Jump Into The Fog.mp3");
-add_music_cue("C:/Users/Ben/Downloads/The Wombats - Jump Into The Fog.mp3");
+add_music_cue("C:/Users/Ben/OneDrive/Downloads/The Killers - Spaceman.mp3");
+add_music_cue("C:/Users/Ben/OneDrive/Downloads/The Killers - Be Still.mp3");
+add_music_cue("C:/Users/Ben/Downloads/The Killers - Be Still.mp3");
+add_music_cue("C:/Users/Ben/Downloads/The Killers - Be Still.mp3");
+add_music_cue("C:/Users/Ben/OneDrive/Downloads/The Wombats - Jump Into The Fog.mp3");
